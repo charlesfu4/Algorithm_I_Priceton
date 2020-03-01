2e2e397cb20fe09f3e854b63ac84281e317f5b2b
Chu-Cheng Fu <ccfu@kth.se>
2020-03-01 17:28:58 +0100

    Week2_v1

---
+++ Week2_Queue/Deque.java
@@ -0,0 +1,212 @@
+/* *****************************************************************************
+ *  Name: Chu-Cheng Fu
+ *  Date: 29-02-2020
+ *  Description: Queue algorithm assignment - Deque
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    private Item[] deq;
+    private int N = 0;
+    private int head;
+    private int tail;
+
+    // construct an empty deque
+    public Deque() {
+        deq = (Item[]) new Object[4];
+        head = 2;
+        tail = 2;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return N;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        //corner case
+        if (item == null)
+            throw new IllegalArgumentException();
+        if (head <= 1)
+            resizel(deq.length * 2);
+
+        if (deq[head] == null)
+            deq[head] = item;
+        else {
+            head--;
+            deq[head] = item;
+        }
+        N++;
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        //corner case
+        if (item == null)
+            throw new IllegalArgumentException();
+        if (tail >= deq.length - 1)
+            resizel(deq.length * 2);
+
+        if (deq[tail] != null) {
+            tail++;
+            deq[tail] = item;
+        }
+        else
+            deq[tail] = item;
+        N++;
+    }
+
+    //resize the array
+    private void resizel(int cap) {
+        if (cap <= 0)
+            throw new IllegalArgumentException();
+        Item[] copy = (Item[]) new Object[cap];
+        for (int i = 0; i < N; i++) {
+            copy[cap / 4 + i] = deq[head + i];
+        }
+        head = cap / 4;
+
+        if (N >= 1)
+            tail = cap / 4 + N - 1;
+        else
+            tail = head;
+        deq = copy;
+
+    }
+
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        //corner case
+        if (deq[head] == null)
+            throw new NoSuchElementException();
+        N--;
+        Item temp = deq[head];
+        if (head == deq.length - 1) {
+            deq[head] = null;
+            return temp;
+        }
+        else {
+            if (deq[head + 1] != null) {
+                deq[head] = null;
+                head++;
+                return temp;
+            }
+            else {
+                deq[head] = null;
+                return temp;
+            }
+        }
+
+    }
+
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        //corner case
+        if (deq[tail] == null)
+            throw new NoSuchElementException();
+        N--;
+        Item temp = deq[tail];
+        if (tail == 0) {
+            deq[tail] = null;
+            return temp;
+
+        }
+        else {
+            if (deq[tail - 1] != null) {
+                deq[tail] = null;
+                tail--;
+                return temp;
+            }
+            else {
+                deq[tail] = null;
+                return temp;
+            }
+        }
+
+
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        private int start = head;
+
+        public boolean hasNext() {
+            return (start <= tail) && (N > 0);
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (deq[start] == null)
+                throw new NoSuchElementException();
+            if (start < deq.length - 1)
+                return deq[start++];
+            else
+                return deq[start];
+        }
+
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        Deque<Integer> d = new Deque<>();
+        StdOut.println("AddFirst: ");
+        d.addFirst(StdIn.readInt());
+        StdOut.println("Removing the last element: " + d.removeLast());
+
+        while (StdIn.hasNextLine()) {
+
+
+            StdOut.println("The size of deque is: " + d.size());
+            StdOut.println("The deque is empty? " + d.isEmpty());
+            StdOut.println("AddFirst: ");
+            d.addFirst(StdIn.readInt());
+            StdOut.println("The size of deque is: " + d.size());
+            StdOut.println("The deque is empty? " + d.isEmpty());
+            StdOut.println("Removing the last element: " + d.removeLast());
+            StdOut.println("AddFirst: ");
+
+            StdOut.println("AddLast: ");
+            d.addLast(StdIn.readInt());
+            StdOut.println("The size of deque is: " + d.size());
+            StdOut.println("The deque is empty? " + d.isEmpty());
+
+
+            StdOut.println("Removing the first element: " + d.removeFirst());
+
+            StdOut.println("Iterate through elements from head to tail:");
+            Iterator<Integer> i = d.iterator();
+            while (i.hasNext()) {
+                StdOut.println(i.next());
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Week2_Queue/RandomizedQueue.java
@@ -0,0 +1,168 @@
+/* *****************************************************************************
+ *  Name: Chu-Cheng Fu
+ *  Date: 29-02-2020
+ *  Description: Queue algorithm assignment - RandomizedQueue
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] rq;
+    private int N = 0;
+    private int idx;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        rq = (Item[]) new Object[4];
+        idx = 0;
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return N;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException();
+        if (idx >= rq.length - 1)
+            resize(rq.length * 2);
+        if (rq[idx] == null)
+            rq[idx] = item;
+        else
+            rq[++idx] = item;
+        N++;
+    }
+
+    private void resize(int cap) {
+        if (cap <= 0)
+            throw new IllegalArgumentException();
+        Item[] copy = (Item[]) new Object[cap];
+        for (int i = 0; i < N; i++) {
+            copy[i] = rq[i];
+        }
+        rq = copy;
+    }
+
+
+    // remove and return a random item
+    public Item dequeue() {
+        if (N <= 0)
+            throw new NoSuchElementException();
+        int ridx = StdRandom.uniform(N);
+        if (rq[ridx] == null)
+            throw new NoSuchElementException();
+        N--;
+        Item temp = rq[ridx];
+        // replace the vacancy
+        if (ridx != idx) {
+            rq[ridx] = rq[idx];
+            rq[idx] = null;
+            if (idx > 0)
+                idx--;
+        }
+
+        else {
+            rq[idx] = null;
+            if (idx > 0)
+                idx--;
+        }
+        return temp;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        if (N <= 0)
+            throw new NoSuchElementException();
+        int ridx = StdRandom.uniform(N);
+        return rq[ridx];
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomIterator();
+    }
+
+    private class RandomIterator implements Iterator<Item> {
+        private int[] walklist = StdRandom.permutation(N);
+        private int pidx = 0;
+
+        public boolean hasNext() {
+            return pidx < N;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            return rq[walklist[pidx++]];
+        }
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+        RandomizedQueue<Integer> r = new RandomizedQueue<>();
+        StdOut.println("Enqueue: ");
+        r.enqueue(StdIn.readInt());
+        StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+        StdOut.println("The size of randomizedqueue is: " + r.size());
+
+        while (StdIn.hasNextLine()) {
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("Enqueue: ");
+            r.enqueue(StdIn.readInt());
+            StdOut.println("The randomizedqueue is empty? " + r.isEmpty());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+
+            StdOut.println("Iterate through elements randomly:");
+            Iterator<Integer> i = r.iterator();
+            while (i.hasNext()) {
+                StdOut.println(i.next());
+            }
+
+            StdOut.println("Dequeue: " + r.dequeue());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("sample= " + r.sample());
+            StdOut.println("Dequeue: " + r.dequeue());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+            StdOut.println("sample= " + r.sample());
+
+            StdOut.println("sample= " + r.sample());
+            StdOut.println("The size of randomizedqueue is: " + r.size());
+
+        }
+    }
+
+}
